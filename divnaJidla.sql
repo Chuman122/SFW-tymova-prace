3fa2a7aa8587d4ad4986694af5755de36c43b9a7
CHUMAN Pavel (122) <chuman122@trebesin.cz>
2025-04-02 11:21:25 +0200

    fuj tajibl

--- divnaJidla.sql
+++ divnaJidla.sql
@@ -8,4 +8,12 @@ CREATE TABLE divna_jidla (
 INSERT INTO divna_jidla (jmeno, jidlo, prezil)
 VALUES
   ('Tomáš Černý', 'Hamburger s banánem', TRUE),
-  ('Tomáš Černý', 'Monte jogurt s okurkou', TRUE);
+  ('Tomáš Černý', 'Monte jogurt s okurkou', TRUE)
+  ('Vorlík, Chuman', 'držkovka', TRUE),
+  ('Vorlík', 'něco co připomíná bramborovou kaši ze školní jídelny', FALSE),
+  ('Vorlík, Chuman', 'husí embrio', FALSE),
+  ('Vorlík, Chuman', 'Prdelačka', TRUE),
+  ('Chuman', 'Čínská polívka vařená teplou vodou z umyvadla (improvizace na intru)', TRUE),
+  ('Chuman', 'Tousťák s kečupem (intr)', TRUE),
+  ('Vorlíček', 'Vaená čekanka', TRUE),
+  ('Chuman', 'Topinambur', TRUE);
