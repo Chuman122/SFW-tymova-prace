f9f0433fe490c9f67d46b22dffbcd3c32344b889
Filip Kuchynka <kuchynka122@trebesin.cz>
2025-04-02 11:19:21 +0200

    Happy halloween

--- divnaJidla.sql
+++ divnaJidla.sql
@@ -8,4 +8,7 @@ CREATE TABLE divna_jidla (
 INSERT INTO divna_jidla (jmeno, jidlo, prezil)
 VALUES
   ('Tomáš Černý', 'Hamburger s banánem', TRUE),
-  ('Tomáš Černý', 'Monte jogurt s okurkou', TRUE);
+  ('Tomáš Černý', 'Monte jogurt s okurkou', TRUE),
+  ('Filip Kuchynka', 'Jahodový vitriol s nakrájeným eidam 3,14', NOT SURE),
+  ('Prokop Spálenka', 'Zmrzlina z kyselých okurek s čoko polevou', TRUE),
+  ;
