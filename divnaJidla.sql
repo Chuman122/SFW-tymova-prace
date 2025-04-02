84e83ac60e67962c258375f285629e95e55630c9
SOUKUP Dominik (122) <soukup122@trebesin.cz>
2025-04-02 11:11:34 +0200

    Přidání nechutných jídel

--- divnaJidla.sql
+++ divnaJidla.sql
@@ -9,3 +9,8 @@ INSERT INTO divna_jidla (jmeno, jidlo, prezil)
 VALUES
   ('Tomáš Černý', 'Hamburger s banánem', TRUE),
   ('Tomáš Černý', 'Monte jogurt s okurkou', TRUE);
+  ('Dominik Soukup', 'Halušky s brinzou', TRUE);
+  ('Jakub Matoušek', 'UHO(univerzalni hneda omacka)', TRUE);
+  ('Jakub Matoušek', 'Banány', FALSE);
+  ('Dominik Soukup', 'Špagety s cuketou', TRUE);
+  ('Dominik Soukup', 'Prdelačka(polevka co se dela na zabijačkách)', FALSE);
