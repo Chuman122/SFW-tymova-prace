fd0ec5026e861a4de181dc1e8dca4517f2df9e22
Marek Svačina <svacina122@trebesin.cz>
2025-04-02 11:22:46 +0200

    Zapsání jídel

--- divnaJidla.sql
+++ divnaJidla.sql
@@ -9,3 +9,8 @@ INSERT INTO divna_jidla (jmeno, jidlo, prezil)
 VALUES
   ('Tomáš Černý', 'Hamburger s banánem', TRUE),
   ('Tomáš Černý', 'Monte jogurt s okurkou', TRUE);
+  ('Marek Svačina', 'Prášek na polévku bez vody', TRUE);
+  ('Marek Svačina', 'Pizza s rybou a ananasem', TRUE);
+  ('Marek Svačina', 'Sušenka pro psy', TRUE);
+  ('Marek Svačina', 'Syrové kuře', TRUE);
+  ('Marek Svačina', 'Nevařené těstoviny', TRUE);
