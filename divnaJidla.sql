2fffdb1fa5d4f7528c3925447f89458551eaf21c
interfxrence <64658350+interfxrence@users.noreply.github.com>
2025-04-02 11:24:25 +0200

    Update divnaJidla.sql

--- divnaJidla.sql
+++ divnaJidla.sql
@@ -1,17 +1,6 @@
-CREATE TABLE divna_jidla (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  jmeno VARCHAR(50),
-  jidlo TEXT,
-  prezil BOOLEAN
-);
-
-INSERT INTO divna_jidla (jmeno, jidlo, prezil)
-VALUES
-  ('Tomáš Černý', 'Hamburger s banánem', TRUE),
-  ('Tomáš Černý', 'Monte jogurt s okurkou', TRUE);
-  ('Volodymyr Siniaiev', 'Slaninová mrzlina', TRUE);
-  ('Volodymyr Siniaiev', 'Kuřecí prst', FALSE);
-  ('Volodymyr Siniaiev', 'Ananasová pizza', TRUE);
-  ('Volodymyr Siniaiev', 'Syrové vejce', FALSE);
-  ('Volodymyr Siniaiev', 'Smažený švab', FALSE);
+  ('Volodymyr Siniaiev', 'Slaninová mrzlina', TRUE),
+  ('Volodymyr Siniaiev', 'Kuřecí prst', FALSE),
+  ('Volodymyr Siniaiev', 'Ananasová pizza', TRUE),
+  ('Volodymyr Siniaiev', 'Syrové vejce', FALSE),
+  ('Volodymyr Siniaiev', 'Smažený švab', FALSE),
   
