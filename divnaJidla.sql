e358c7849987763dd84226c5487c6872843f4932
WLODARCZYK David (122) <wlodarczyk122@trebesin.cz>
2025-04-02 11:14:28 +0200

    Výběr dobrot

--- divnaJidla.sql
+++ divnaJidla.sql
@@ -7,5 +7,8 @@ CREATE TABLE divna_jidla (
 
 INSERT INTO divna_jidla (jmeno, jidlo, prezil)
 VALUES
-  ('Tomáš Černý', 'Hamburger s banánem', TRUE),
-  ('Tomáš Černý', 'Monte jogurt s okurkou', TRUE);
+  ('Oliver Mandlík', 'Koprovka s kečupem (rajská)', TRUE),
+  ('David Wlodarczyk', 'Prokopova svačina s nejakým plesnivým sýrem, asi to byl eidam dvacítka (bolí mě bříško :3)', FALSE);
+  ('Jan Zahnáš', 'Syrové/krmné brambory ze školní jídelny', TRUE);
+  ('Jan Zahnáš', 'Sekyrková polévka', TRUE);
+  ('Jan Zahnáš', 'Instantní čínská polévka s příchutí střelného prachu', TRUE);
