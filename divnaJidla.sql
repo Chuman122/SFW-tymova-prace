d1a35e5319ae8cc37a310abe28c91cba8477b3f2
Petr <petrkunc133@gmail.com>
2025-04-02 11:10:48 +0200

    Mnamky mnam mnam mnam

--- divnaJidla.sql
+++ divnaJidla.sql
@@ -9,3 +9,8 @@ INSERT INTO divna_jidla (jmeno, jidlo, prezil)
 VALUES
   ('Tomáš Černý', 'Hamburger s banánem', TRUE),
   ('Tomáš Černý', 'Monte jogurt s okurkou', TRUE);
+  ('Petr Kunc', 'Kyselá Okurka s Tatarkou', TRUE);
+  ('Maksym Polyvoda', 'Mochito s citronovou zmrzlinou', TRUE);
+  ('Marek Krejbich', 'Hrachová kaše s uzeným masem', TRUE);
+  ('Petr Kunc', 'Byčí jazyk', TRUE);
+  ('Petr Kunc', 'Byčí žlázy', TRUE);
